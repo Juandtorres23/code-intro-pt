6ad9491943030bfe93c234d18c083fc8ab5c8c70
Juan Torres Reyes <juandtorres23@yahoo.com>
2022-07-02 16:29:00 -0400

    initial commit

---
+++ codeIntro.js
@@ -0,0 +1,34 @@
+// this is a comment
+
+const firstName = 'Juan';
+let lastName = 'Torres';
+var age = 28;
+
+console.log(firstName, lastName);
+// console.log(firstName, lastName);
+console.log(weight);
+
+weight = 999;
+console.log(weight);
+
+let x = 4
+let y = 5
+let z = 6
+const sum = x + y;
+console.log(sum)
+
+const product = y * z;
+console.log(product);
+
+const newProduct = sum + product;
+console.log(newProduct);
+
+const string1 = 1;
+const string2 = "two";
+const string3 = "3 + 3";
+
+console.log(string3);
+
+
+
+
